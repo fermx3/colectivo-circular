07c9ff844788c68d69c3e2278c3843c53fd7fcde
Fernando Manuel Rios Alcantara <fernandomanuelriosalcantara@iMac-de-Fernando.local>
2022-07-05 14:57:36 -0500

    Render of entries working

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     //window.location.href = 'index.html'
 
-    $('#menu').click(function(){
-        if($('#menu').hasClass('active')){
-            $('#menu').find('.item').each(function(){
+    $('#menu').click(function () {
+        if ($('#menu').hasClass('active')) {
+            $('#menu').find('.item').each(function () {
                 $(this).removeClass('active');
             });
             $('#menu').removeClass('active');
@@ -12,18 +12,18 @@ $(document).ready(function(){
             $('.icon-menu').removeClass('active');
         }
     });
-    $('.icon-menu').click(function(){
-        
-        if($('#menu').hasClass('active')){
+    $('.icon-menu').click(function () {
+
+        if ($('#menu').hasClass('active')) {
             $(this).removeClass('active');
-            $('#menu').find('.item').each(function(){
+            $('#menu').find('.item').each(function () {
                 $(this).removeClass('active');
             });
             $('#menu').removeClass('active');
             $('body').css('overflow', 'initial');
-        }else{
+        } else {
             $(this).addClass('active');
-            $('#menu').find('.item').each(function(){
+            $('#menu').find('.item').each(function () {
                 $(this).addClass('active');
             });
             $('body').css('overflow', 'hidden');
@@ -32,9 +32,9 @@ $(document).ready(function(){
     });
 
 
-   // animation au clic
+    // animation au clic
 
-    $(window).click(function(event) {
+    $(window).click(function (event) {
 
         const expl = document.createElement('span');
         expl.className = 'clickAnim';
@@ -47,12 +47,29 @@ $(document).ready(function(){
         }, 500)
     });
 
-    $('.close').click(function(){
+    $('.close').click(function () {
         $('.modal').css('display', 'none');
     });
 
-    $('.card-content').click(function(){
+    $('.card-content').click(function () {
         $('.modal').css('display', 'block');
+    });
+
+    //Display titles and content of blog posts
+
+    $(".card-content").click(function () {
+        const title = document.querySelector("#post-title");
+        const content = document.querySelector("#post-content");
+
+        if (document.querySelector("#page-title").innerText == "BLOG") {
+            title.innerHTML = $(this.children)[1].innerHTML;
+            content.innerHTML = $(this.children)[2].innerHTML;
+        }
+
+        if (document.querySelector("#page-title").innerText == "INICIATIVA") {
+            title.innerHTML = $(this.children)[0].innerHTML;
+            content.innerHTML = $(this.children)[1].innerHTML;
+        }
     });
 
 });
