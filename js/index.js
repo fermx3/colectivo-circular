5e0f28301291fec93d0d51a14fbb5a162f38b897
Fernando Manuel Rios Alcantara <fernandomanuelriosalcantara@iMac-de-Fernando.local>
2022-07-08 19:40:28 -0500

    Add blog links

--- js/index.js
+++ js/index.js
@@ -52,18 +52,30 @@ $(document).ready(function () {
     });
 
     $('.card-content').click(function () {
-        $('.modal').css('display', 'block');
+        //$('.modal').css('display', 'block');
+
+        // Activate links to blogposts
+        if (document.querySelector(".work")) {
+            const urlRaw = $(this.children)[1].innerText;
+            const urlNormal = urlRaw.normalize("NFD").replace(/\p{Diacritic}/gu, "");
+            const urlLowerCase = urlNormal.toLowerCase();
+            const urlNoSymbols = urlLowerCase.replace(/[^\w\s]/gi, '');
+            const url = urlNoSymbols.split(' ').join('-');
+
+            window.location = `https://colectivocircular.com/blog/${url}`;
+        }
 
         //Display titles and content of blog posts
         const title = document.querySelector("#post-title");
         const content = document.querySelector("#post-content");
-
-        if (document.querySelector(".work")) {
-            title.innerHTML = $(this.children)[1].innerHTML;
-            content.innerHTML = $(this.children)[2].innerHTML;
-        }
+        
+        // if (document.querySelector(".work")) {
+        //     title.innerHTML = $(this.children)[1].innerHTML;
+        //     content.innerHTML = $(this.children)[2].innerHTML;
+        // }
 
         if (document.querySelector(".events")) {
+            $('.modal').css('display', 'block');
             title.innerHTML = $(this.children)[0].innerHTML;
             content.innerHTML = $(this.children)[1].innerHTML;
         }
