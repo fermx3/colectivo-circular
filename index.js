33102cb1794dfc4350c518859e31e0b6e8154777
Fernando Manuel Rios Alcantara <fernandomanuelriosalcantara@iMac-de-Fernando.local>
2022-07-12 16:55:02 -0500

    Reorder directories for nodejs

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get("/blog", function (req, res) {
+  res.sendFile(__dirname + '/pages/work.html');
+})
+
+app.get("/about", function (req, res) {
+  res.send("My name is Fernando. <br>♥︎ I am a web developer. ♥︎")
+})
+
+app.listen(port, function() {
+  console.log("Server started on port " + port);
+});
